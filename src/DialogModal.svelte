36188c1ec48d85c0b3974d86c77a08b5ac5ff3a0
Lukáš Kovář <lkov@post.cz>
2022-02-12 16:48:25 +0100

    Dialog zIndex

--- src/DialogModal.svelte
+++ src/DialogModal.svelte
@@ -6,7 +6,7 @@
 
   onMount(async () => {
     const style = document.createElement("style");
-    style.innerHTML = `@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');`;
+    style.innerHTML = `@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');`
     document.body.appendChild(style);
     return () => style.remove();
 
@@ -162,6 +162,7 @@
 </div>
 
 <style>
+
   .input {
   }
   * {
@@ -179,6 +180,7 @@
     position: fixed;
     top: 0;
     left: 0;
+    z-index: 100000;
   }
 
   .tangle-msg-box-dialog {
