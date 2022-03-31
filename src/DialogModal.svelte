1cd89afb81631f66ade64bfb93471f5bca83db6f
Lukáš Kovář <lkov@post.cz>
2022-03-31 13:31:22 +0200

    fix focus

--- src/DialogModal.svelte
+++ src/DialogModal.svelte
@@ -22,14 +22,22 @@
     console.log(inputtype);
 
     value = value || defaultvalue;
-    component.focus();
+    try {
+      component.focus();
+    } catch {
+      // ignore
+    }
     const style = document.createElement("style");
     style.innerHTML = `@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');`;
     component.appendChild(style);
 
     setTimeout(() => {
       if (inputtype.match(/text|email|tel|url/)) {
-        inputField && inputField.focus();
+        try {
+          inputField && inputField.focus();
+        }catch {
+          // ignore
+        }
         inputField && inputField.click();
         inputField &&
           setTimeout(() => {
